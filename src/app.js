6b3597c0362ab8fc6259953bde0acff946499ba2
Diana Garbuzova <lessarea@gmail.com>
2018-08-16 01:53:41 +0300

    task were done v1.0

---
+++ src/app.js
@@ -0,0 +1,23 @@
+angular
+  .module('myApp', [])
+  .controller('containerCtrl', function() {
+    this.value = 'not changed';
+    this.click = function(newValue) {
+      this.value = newValue;
+    }
+  })
+  .directive('myContainer', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      controller: 'containerCtrl',
+      controllerAs: 'container',
+      templateUrl: 'my-container.html'
+    }
+  })
+  .directive('myButton', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'my-button.html'
+    }
+  });
